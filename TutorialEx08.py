8d487851183b2553037ff91e5d84d359ff731da7
Alicia Susi <asusi@nd.edu>
2018-11-02 09:23:12 -0400

    Updated graph with Quarters but no lines appearing

--- TutorialEx08.py
+++ TutorialEx08.py
@@ -4,30 +4,43 @@ Created on Thu Nov  1 12:54:34 2018
 
 @author: Alicia
 """
-
-import numpy
+#Question 1
 import pandas
-from plotnine import *
 import matplotlib.pyplot as plt
 
-game=pandas.read_csv("UWvMSU_1-22-13.txt",sep="\t",header=0)
+df=pandas.read_csv("UWvMSU_1-22-13.txt",sep="\t",header=0)
+print(df)
+df["UWscore"]=df.loc[df["team"]=="UW","score"].cumsum()
+df["MSUscore"]=df.loc[df["team"]=="MSU","score"].cumsum()
+df2=df.fillna(method="ffill")
+df2=df.fillna(0)
+print(df2)
+sorted_data=df2.sort_values('time')
+print(sorted_data)
+df2.loc[df2['time'].between(0,10.99),'Quarter']='First'
+df2.loc[df2['time'].between(11,20.99),'Quarter']='Second'
+df2.loc[df2['time'].between(21,30.99),'Quarter']='Third'
+df2.loc[df2['time'].between(31,40.99),'Quarter']='Fourth'
+print(df2)
+plt.plot("Quarter","UWscore","r-","Quarter","MSUscore","g-")
 
-a=ggplot(game,aes(x="time",y="score"))
-a+geom_point()+coord_cartesian()+theme_classic()
-plt.plot(time,UWscore,'r-',time,MSUscore,'g-')
+#Question 2 - Guess My Number
+import random
 
-
-#Question 2 - Guess my Number
-n = numpy.random.choice(1, 99)
-guess = int(numpy.random.choice("Enter an integer from 1 to 99: "))
+n = random.randint(1, 100)
+print('I am thinking of a number between 1 and 100.')
+guess = input("Take a guess ")
+guess = int(guess)
 while n != "guess":
-    print
+    print(guess)
     if guess < n:
-        print("Guess is too low")
-        guess = int(numpy.random.choice("Enter an integer from 1 to 99: "))
+        print('Your guess is too low.')
+        guess = input("Take a guess ")
+        guess = int(guess)
     elif guess > n:
-        print("Guess is too high")
-        guess = int(numpy.random.choice("Enter an integer from 1 to 99: "))
+        print('Your guess is too high.')
+        guess = input("Take a guess ")
+        guess = int(guess)
     else:
         print("You guessed right!")
         break
